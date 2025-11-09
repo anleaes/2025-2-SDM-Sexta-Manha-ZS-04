f92c06da6048ac9701f1ab521376241d5ff89cd6
Gabriel Fleischmann Funck <gabriel.funck45@gmail.com>
2025-11-09 20:11:33 -0300

    Criação do modelo de salas

--- apps/salas/models.py
+++ apps/salas/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from cinemas.models import Cinema
 
 # Create your models here.
+class Sala(models.Model):
+    lotacao_maxima = models.IntegerField('Lotação Máxima')
+    assentos_normais = models.IntegerField('Assentos Normais')
+    assentos_acessiveis = models.IntegerField('Assentos Acessíveis')
+    cinema = models.ForeignKey(Cinema, on_delete=models.CASCADE)
+ 
+    class Meta:
+        verbose_name = 'Sala'
+        verbose_name_plural = 'Salas'
+        ordering =['id']
+
+    def __str__(self):
+        return self.lotacao_maxima
