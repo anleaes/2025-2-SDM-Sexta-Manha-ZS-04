ca8a2b4e378b07fab08d277dfeabf6e4c234c374
Gabriel Fleischmann Funck <gabriel.funck45@gmail.com>
2025-11-09 20:40:21 -0300

    Configuração das views de filmes

--- apps/filmes/views.py
+++ apps/filmes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Filme
+from .serializer import FilmeSerializer
 
 # Create your views here.
+class FilmeViewSet(viewsets.ModelViewSet):
+    queryset = Filme.objects.all()
+    serializer_class = FilmeSerializer  
