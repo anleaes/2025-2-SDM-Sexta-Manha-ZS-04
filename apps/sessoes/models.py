59df7479a1a06a5765e0ff968eaf3cd44fafae8d
Gabriel Fleischmann Funck <gabriel.funck45@gmail.com>
2025-11-10 20:22:33 -0300

    Criação do modelo do app sessoes

--- apps/sessoes/models.py
+++ apps/sessoes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from salas.models import Sala
+from filmes.models import Filme
 
 # Create your models here.
+class Sessao(models.Model):
+    horario_de_inicio = models.TimeField('Horário de Início')
+    tipo_de_exibicao = models.CharField('Exibição', max_length=50)
+    sala = models.ForeignKey(Sala, on_delete=models.CASCADE)
+    filme = models.ForeignKey(Filme, on_delete=models.CASCADE)
+ 
+    class Meta:
+        verbose_name = 'Sessão'
+        verbose_name_plural = 'Sessões'
+        ordering =['id']
+
+    def __str__(self):
+        return self.horario_de_inicio
