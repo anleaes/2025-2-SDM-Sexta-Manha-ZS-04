dfe741926aa3e215adf939629dee3879ce52f5d6
Gabriel Fleischmann Funck <gabriel.funck45@gmail.com>
2025-11-13 00:29:22 -0300

    Criação do modelo de pagamentos

--- apps/pagamentos/models.py
+++ apps/pagamentos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Pagamento(models.Model):
+    nome = models.CharField('Nome')
+    tipo = models.CharField('Tipo', max_length=50, default='À Vista', choices=[
+        ('a_vista', 'Vista'),
+        ('parcelado', 'Parcelado'),
+        ('voucher', 'Voucher'),
+    ]
+    )
+    desconto = models.DecimalField('Desconto')
+    ativo = models.BooleanField('Ativo')
+ 
+    class Meta:
+        verbose_name = 'Pagamento'
+        verbose_name_plural = 'Pagamentos'
+        ordering =['id']
+
+    def __str__(self):
+        return f"{self.nome} - {self.tipo})"
