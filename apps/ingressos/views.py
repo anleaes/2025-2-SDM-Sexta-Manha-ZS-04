de50c65292846a34830d2fc89aecdb823a718dc9
Gabriel Fleischmann Funck <gabriel.funck45@gmail.com>
2025-11-14 00:24:30 -0300

    Inclusão do filtro por tipo no app ingressos

--- apps/ingressos/views.py
+++ apps/ingressos/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Ingresso
 from .serializer import IngressoSerializer
 
 # Create your views here.
 class IngressoViewSet(viewsets.ModelViewSet):
     queryset = Ingresso.objects.all()
-    serializer_class = IngressoSerializer  
+    serializer_class = IngressoSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['tipo'] 
