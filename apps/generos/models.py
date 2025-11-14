dbb713e635027eaac515b1027ae4cb7fe4164f7e
gabrielfleischmann <gabriel.funck45@gmail.com>
2025-11-14 10:06:34 -0300

    Declaração do meta e ToString do app generos

--- apps/generos/models.py
+++ apps/generos/models.py
@@ -6,3 +6,12 @@ class Genero(models.Model):
     descricao = models.TextField('Descrição', max_length=400)
     clasificacao_indicativa = models.CharField('Classificação Indicativa', max_length=50)
     pais_de_origem = models.CharField('País de Origem', max_length=50)
+
+    class Meta:
+            verbose_name = 'genero'
+            verbose_name_plural = 'generos'
+            ordering =['id']
+
+
+    def __str__(self):
+        return f"{self.nome} (Classificação Indicativa: {self.clasificacao_indicativa})"
