54fa17bd64d9a94b90807c2624ebe3bb5835fe01
Gabriel Fleischmann Funck <gabriel.funck45@gmail.com>
2025-11-13 21:34:53 -0300

    Declaração dos atributos de ingressos

--- apps/ingressos/models.py
+++ apps/ingressos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from sessoes.models import Sessao
+from clientes.models import Cliente
 
 # Create your models here.
+class Ingresso(models.Model):
+    tipo = models.CharField('Tipo', max_length=50, default='Inteira', choices=[
+        ('inteira', 'Inteira'),
+        ('meia_estudante', 'Meia — Estudante'),
+        ('meia_pcd', 'Meia — PCD'),
+        ('promocional', 'Promocional'),
+        ('infantil', 'Infantil'),
+        ('cortesia', 'Cortesia'),
+    ])
+    preco =  models.DecimalField('Preço', max_digits=5, decimal_places=2, default=0)
+    sessao = models.ForeignKey(Sessao, on_delete=models.CASCADE)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)   
+   
