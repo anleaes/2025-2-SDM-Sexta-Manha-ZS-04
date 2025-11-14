df616a765c526ef3995785f1f8cf730e8f1ba165
gabrielfleischmann <gabriel.funck45@gmail.com>
2025-11-14 10:12:36 -0300

    Configuração das views do app generos

--- apps/generos/views.py
+++ apps/generos/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Genero
 from .serializer import GeneroSerializer
+
 # Create your views here.
+class GeneroViewSet(viewsets.ModelViewSet):
+    queryset = Genero.objects.all()
+    serializer_class = GeneroSerializer  
