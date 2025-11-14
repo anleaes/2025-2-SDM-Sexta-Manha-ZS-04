618184b55767e46b212d149d6d204865b743ab4b
gabrielfleischmann <gabriel.funck45@gmail.com>
2025-11-14 10:05:35 -0300

    Declaração dos atributos do app generos

--- apps/generos/models.py
+++ apps/generos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Genero(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+    descricao = models.TextField('Descrição', max_length=400)
+    clasificacao_indicativa = models.CharField('Classificação Indicativa', max_length=50)
+    pais_de_origem = models.CharField('País de Origem', max_length=50)
