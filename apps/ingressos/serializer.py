8ae0e578b72d4a8a722004b7003e15f6f9e45976
Gabriel Fleischmann Funck <gabriel.funck45@gmail.com>
2025-11-13 21:37:40 -0300

    Criação do serializer

--- apps/ingressos/serializer.py
+++ apps/ingressos/serializer.py
@@ -1,2 +1,7 @@
 from .models import Ingresso
 from rest_framework import serializers
+
+class IngressoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingresso
+        fields = '__all__'
